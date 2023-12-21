7c4c6b5bc97dede5f450a311f1e48d66e0573b3d
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-12-21 11:23:22 +0300

    added exception for task1

--- src/main/java/aston/qa/AlgorithmTests.java
+++ src/main/java/aston/qa/AlgorithmTests.java
@@ -5,32 +5,42 @@ import java.util.*;
 public class AlgorithmTests {
 
     public static void task1() {
-        System.out.println("Введите число от 1 до 10");
+
         Scanner scan = new Scanner(System.in);
-        int numberInput = scan.nextInt();
-        if (numberInput > 7) {
-            System.out.println("Привет");
-        } else {
-            System.out.println("Пока");
-        }}
+        try {
+            System.out.println("Введите число от 1 до 10");
+            int numberInput = scan.nextInt();
+            if (numberInput > 7) {
+                System.out.println("Привет");
+            } else {
+                System.out.println("Пока");
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Ошибка ввода, введите число цифрами2");
+        }
+    }
 
     public static void task2() {
+
+
         System.out.println("Введите имя, допустим, Вячеслав");
-        Scanner scan = new Scanner(System.in);
-        String nameInput = scan.nextLine();
+        Scanner scan1 = new Scanner(System.in);
+        String nameInput = scan1.nextLine();
         if (nameInput.equals("Вячеслав")) {
             System.out.println("Привет, Вячеслав");
         } else {
             System.out.println("Нет такого имени");
-        }}
+        }
+    }
 
     public static void task3() {
-        int[] numbers = {1,99,100,33,56,3};
+        int[] numbers = {1, 99, 100, 33, 56, 3};
 
-        for (int a: numbers)
-            if (a % 3 ==0)
-            {System.out.println(a);}}
-
+        for (int a : numbers)
+            if (a % 3 == 0) {
+                System.out.println(a);
+            }
+    }
 
     public static void main(String[] args) {
         task1();
@@ -38,3 +48,9 @@ public class AlgorithmTests {
         task3();
     }
 }
+
+
+
+
+
+
